dc31bf631c649761839cb0e8593749880cdaa4cb
Miloš Kajtez <kajtez@gmail.com>
2020-12-23 16:56:52 +0100

    Add commets and optimaze

--- P3 - Largest prime factor.py
+++ P3 - Largest prime factor.py
@@ -1,16 +1,20 @@
-maxNummber = 600851475143
+#The prime factors of 13195 are 5, 7, 13 and 29.
+#What is the largest prime factor of the number 600851475143 ?
+
+maxNumber = 600851475143
 primeFactors = []
 
+# Check if number is prime number
 def IsPrime(number):
-    isPrime = True
-    i = number - 1
-    while i > 1:
+    i =2
+    while i < number:
+        # If number is devidable( modulo is 0)  it's not prime
         if((number % i) == 0):
-            isPrime = False
-            break
-        i-=1
-    return isPrime
+            return False
+        i+=1
+    return True
 
+# Get next prime number
 def getNextPrime(number):
     x = number
     while True:
@@ -18,15 +22,19 @@ def getNextPrime(number):
         if(IsPrime(x)):
             return x
 
-curent = maxNummber
+curent = maxNumber
 curentPrime = 2
 
+
 while curent != 1:
+    # If prime is prime factor devide, add it to list and start from 2
     if ((curent % curentPrime) == 0):
         primeFactors.append(curentPrime)
         curent = curent / curentPrime
         curentPrime = 2
+    # If prime is not prime factor, get next prime
     curentPrime = getNextPrime(curentPrime)
 
+# Get biggest prime fator from list
 result = max(primeFactors)
 print("Result:", result)
